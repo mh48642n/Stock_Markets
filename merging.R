6088098f219483dde672936893fda4ee08f44945
mh48642n <mh48642n@pace.edu>
2023-10-24 21:47:18 -0400

    Updated code and data

--- merging.R
+++ merging.R
@@ -11,33 +11,34 @@ library(xts)
 library(lubridate)
 
 #puts file names into list object to iterate through it better
-files <- list("macro_tz.xlsx")
+files <- list("macro_tz.xlsx", "debt_ceiling_tz.xlsx", "debt_tz.xlsx")
 #creates data objects and attaches them to better join them
 x <- read_excel("indices_tz.xlsx")
-y <- read_excel("debt_tz.xlsx")
-
 attach(x)
-#x <- transform_weekly(x)
-#y <- transform_weekly(y)
-#l <- transform_weekly(l)
-attach(y)
 
 #function merges data into one sheet
 merge <- function(){
+  #This helps when iterating through the files
+  confirm_ = 1
   #joins x and y dataframes
-  merged <- left_join(x, y, by = "dates")
-  merged <- fortify.zoo(dailyavg_weekly(merged))
-  merged$dates = merged$Index
+  merged <- weekly_(x, confirm_)
+  rownames(merged) <- NULL
+  merged <- merged[,-c(2)]
+  colnames(merged)[1] = "dates"
+  
+  confirm_ = 2
   
   # iterates through files in working directory and joins them together
   for(items in files){
     k <- read_excel(items)
     attach(k)
-    k <- monthly_weekly(k)
+  
+    k <- weekly_(k, confirm_)
     
     merged <- left_join(merged, k, by = "dates")
-  }
-  merged <- merged[, -c(1, 5, 6, 8, 10, 11)]
+    confirm_ = confirm_ + 1 
+      }
+  
   
   #makes the dates column of type date and formats the dates a specific way 
   merged <- merged %>% mutate(dates = as.Date(dates, format = "%Y-%m-%d")) 
@@ -46,27 +47,62 @@ merge <- function(){
   return(merged)  
 }
 
-#collapses daily data into weekly increments and finds the average value for the week
-dailyavg_weekly <- function(z){
-  z <- z %>% mutate(dates = as.Date(z$dates))
-  z <- z %>% as.xts(order.by = .$dates)  
-  z <- apply.weekly(z, mean)
+#collapses daily/monthly data into weekly increments and finds the average value for the week
+weekly_ <- function(z, confirm_){
+  z = z
+  if(confirm_ == 1){
+    #find the weekly average for different values
+    z <- z %>% mutate(dates = as.Date(z$dates))
+    z <- z %>% as.xts(order.by = .$dates)
+    z <- apply.weekly(z, mean)
+    z <- fortify.zoo(z)
+    print(z)
+    }
+  else if(confirm_ == 2){
+    #finds the week of each first of the month put places it on Friday
+    z <- z %>% mutate(dates = as.Date(z$dates))
+    new_dates <- (ceiling_date(z$dates, unit = "week") + 5)
+    z$dates <- new_dates
+    print(z)
+    }
+  else if(confirm_ == 3 || 4){
+    if(confirm_ == 3){  
+      #finds the week the date of the debt ceiling change
+      #and sets a variable called 
+      z <- z %>% mutate(dates = as.Date(z$dates))
+      new_dates <- (ceiling_date(z$dates, unit = "week") - 2)
+      z <- z %>% add_column(ceiling_dates = z$dates) %>%
+        mutate(dates = new_dates)
+      print(x)
+          }
+    else{
+      #finds the week the debt is raised and then make the week the identifier 
+      #locates the dates when the date was raised
+      z <- z %>% mutate(dates = as.Date(z$dates))
+      new_dates <- (ceiling_date(z$dates, unit = "week") - 2)
+      z <- z %>% mutate(action_dates = dates) %>%
+        mutate(dates = new_dates)
+      print(z)
+      z <- z[z$dates == z$action_dates, ]
+        } 
+      
+    }  
+  
   
   return(z)
 }
-#makes monthly into weekly increments and shifts the root date by 5 days
-monthly_weekly <- function(l){
-  l <- l %>% mutate(dates = as.Date(l$dates))
-  new_dates <- (ceiling_date(l$dates, unit = "week") + 5)
-  l$dates <- new_dates
-  return(l)
-}
+
 
 #returns merged data and stores it in a data object
 data <- merge()
-
+data <- data[, -c(4, 6, 9:12, 14:15)]
 #gives a summary of the dataset
 summary(data)
+str(data)
+
+#gives a correlation matrix
+corr <- data[, c(2:6, 8)] 
+cor(corr, use = "complete.obs")
 
 #to save excel workbook
 #write_xlsx(data, "dataset.xlsx", col_names = TRUE)
