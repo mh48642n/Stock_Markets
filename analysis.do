13c7c525fdaacc8126317987cdf98beece8702cf
mh48642n <mh48642n@pace.edu>
2023-11-26 21:37:50 -0500

    Commit

--- analysis.do
+++ analysis.do
@@ -5,25 +5,71 @@ clear
 cd "C:\Users\marvi\OneDrive\Documents\GitHub\Stock_Markets"
 
 //imports data into stata
-import excel dataset_tsz.xlsx, firstrow clear
-
-//gives the description of each variable 
-codebook
-describe
+import excel stationary_dataset.xlsx, firstrow clear
+import excel monthly_dataset.xlsx, firstrow clear
 
 //declaring data to be time series
-//this points out that the data is daily
-format dates %td
-tsset dates
+//for monthly_dataset
+generate t = tm(2001m1) + _n - 1
+format t %tm
+tsset t
 
+//for daily_dataset
+generate time = td(01jan2001) + _n - 1
+format time %td
+tsset time
+
+//dropping observations 
+drop if missing(s_sp500_p)
+drop dates
+order time
 
 /*BIVARIATE VECTOR AUTO-REGRESSION*/
 	//test if data is stationary
-	
+	dfuller s_russellp, trend
+	dfuller s_sp500_p, trend
 	
 	//Index Prices: Russell 2000 and S&P 500 
-	//Also compare changes to the funds rate to monetary base and then prices
-	var 
+	var s_sp500_p s_funds_rate s_Baa s_T_rate
+	var s_russellp s_funds_rate s_Baa s_T_rate
+     
+	//Debt and rates
+	var s_T_rate s_Baa s_govtheld
+	var s_T_rate s_Baa s_publicheld
+ 
+/*Structural Vector Auto-Regression*/
+/*Short Term*/
+	
+	//Public Debt on Rates
+	//Models the contemporaneous effects across three independent variables
+	matrix A = (1, 0, 0\., 1, 0\., ., 1)
+	matrix B = (., 0, 0\0,. , 0\0, 0, .)
+	
+		/*AB models*/
+		svar s_publicheld s_T_rate s_Baa, aeq(A) beq(B) 
+		
+		/*Counterfactual*/
+		svar s_funds_rate s_T_rate s_Baa, aeq(A) beq(B) 
 
-	//Index Prices and Unemployment
+	//Index Prices and Public Debt
+	//Models the contemporaneous effects between these independent variables
+	matrix A = (1,0,0,0\.,1,0,0\.,.,1,0\.,.,.,1)
+	matrix B = (.,0,0,0\0,.,0,0\0,0,.,0\0,0,0,.)
+	
+		/*AB models*/
+		svar s_publicheld s_T_rate s_Baa s_sp500_p, aeq(A) beq(B)  
+		svar s_publicheld s_T_rate s_Baa s_russellp, aeq(A) beq(B)
+	
+//post diagnostic checks for equations
+/*Casuality tests*/
+vargranger
+
+/*Autocorrelation in the lag orders residuals*/
+varlmar
+
+/*Running FEVD and IRF graphs*/ 
+//IRF on Baa yields
+
+//IRF on T_rate	
+	
 	
--- r_wrangling.R
+++ r_wrangling.R
@@ -14,8 +14,8 @@ files_ <- c("rates_and_currencies.xlsx", "macro_tz.xlsx")
 cpi <- 242.2575
 inflation <- 1.83
 
-#load excel worksheet
-adjust<- function(file, sheet){
+#load excel worksheet and differences data
+#adjust<- function(file, sheet){
   attach(file)
   if(sheet == "MB"){
     file$Mb_millions <- (file$Mb_millions / cpi) * 100
@@ -25,7 +25,7 @@ adjust<- function(file, sheet){
     file$UNRATE <- diff(log(file$UNRATE))
   }
   
-}
+#}
 
 #FOCUS ON GETTING THE DATA IN STATIONARY FORMMMMMM
 for(items in files_){
@@ -35,12 +35,12 @@ for(items in files_){
     
     mb <- adjust(mb, "MB")
     mb <- merge(rt, mb, all = TRUE)
-    write_xlsx(mb, "n_rates_and_currencies.xlsx", col_names = TRUE)
+    #write_xlsx(mb, "n_rates_and_currencies.xlsx", col_names = TRUE)
   }
   if(items == "macros_tz.xlsx"){
     mc <- read_excel(items, sheet = "macros")
     mc <- adjust(mc, "macros")
-    write_xlsx(mc, "n_macro_tz.xlsx", col_names = TRUE)
+    #write_xlsx(mc, "n_macro_tz.xlsx", col_names = TRUE)
     
   }
   # make each excel sheet a time series object 
@@ -49,10 +49,54 @@ for(items in files_){
   # resave it to its original excel file
   # loop it
   
+}
+dt <- read_excel("standard_dataset_tsz.xlsx")
+attach(dt)
+adjust <- function(){
+  dt <- subset(dt, dates < "2020-01-01" & dates > "2000-11-30")
+  dt <- dt[, -c(15, 17:25)]
+   
+  stocks <- dt[, c(1, 2, 4)]
+  stocks <- stocks %>% 
+    group_by(dates = ceiling_date(dates, unit = "month")) %>%
+    summarise(
+      sp500 = mean(sp_500, na.rm = TRUE), 
+      russell = mean(russell_2000, na.rm = TRUE)
+    )
+  debt <- dt[, c(1, 9:11)]
+  debt <- debt %>%
+    group_by(dates = ceiling_date(dates, unit = "month")) %>%
+    summarise(
+      debt = mean(Public_Debt, na.rm = TRUE),
+      govt = mean(Govt_Held, na.rm = TRUE),
+      public = mean(Public_Held, na.rm = TRUE)
+    )
+  yields <- dt[, c(1, 12, 13, 15)]
+  yields <- yields %>%
+    group_by(dates = ceiling_date(dates, unit = "month")) %>%
+    summarise(
+      corp_baa = mean(Baa, na.rm = TRUE),
+      rate_yr = mean(yields_10yr, na.rm = TRUE),
+      funds_rt = mean(eff_fund_rates, na.rm = TRUE)
+      
+    )
+  macro <- dt[, c(1, 8, 14)]
+  macro <- macro %>% 
+    group_by(dates = ceiling_date(dates, unit = "month")) %>%
+    summarise(
+      unrate = mean(UNRATE, na.rm = TRUE),
+      monetary_base = mean(Mb_millions,na.rm = TRUE)
+    
+    )
   
+  macro_debt <- merge(macro, debt, all = TRUE)
+  stock_yields <- merge(stocks, yields, all = TRUE)
+  
+  data <- merge(stock_yields, macro_debt, all = TRUE)
 }
 
-
+data <- subset(data, dates >= "2000-12-01" & "2019-12-01" >= dates)
+write_xlsx(data, "monthly_dataset.xlsx", col_names = TRUE)
 
 
   
