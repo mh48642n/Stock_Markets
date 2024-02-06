70d2a6625aaec3bc6fe68684822c47911160097a
mh48642n <mh48642n@pace.edu>
2024-02-05 22:07:54 -0500

    Full Dataset

---
+++ r_cleaning.R
@@ -0,0 +1,62 @@
+rm(list = ls())
+setwd("C:/Users/marvi/OneDrive/Documents/GitHub/Stock_Markets/excel_sheets")
+getwd()
+
+library(readxl)
+library(tidyverse)
+library(writexl)
+
+#creates a vector with the other files that I want to join together
+files <- c("debt_tz.xlsx", "DGS10.xlsx", "DEXUSEU.xlsx",
+           "rates_and_monetary_base.xlsx","DAAA.xlsx","DFF.xlsx", 
+           "monthly_gdp.xlsx", "M2NS.xlsx", "cpi_release_dates.xlsx", 
+           "meeting_dates.xlsx", "debt_ceiling_tz.xlsx")
+
+#function that attaches and cleans/transforms the data
+join_ <- function(){
+  #reads and attaches the first file 
+  head_ <- inter_att("indices_tsz.xlsx")
+  
+  #attaches each file and joins them together
+  for(value in files){
+   tail_ <- inter_att(value)
+   head_ <- merge(head_, tail_, all = TRUE) 
+  }  
+  
+  #subsets data by date range
+  head_ <- subset(head_, dates > "2000-01-01" & dates < "2023-05-31")
+  
+  return(head_)   
+}
+
+#function attaches and makes formats date in specific way
+inter_att <- function(value){
+  num <- length(excel_sheets(value))
+  print(excel_sheets(value))
+  sheet <- readline("What sheet you want to add: ")
+  a <- read_excel(value, sheet = sheet)
+  
+  while(num > 1){
+    ans <- readline("Do you want to add another sheet(Yes or No): ")
+    if(ans == "Yes"){
+      sheet <- readline("What sheet you want to add: ")
+      b <- read_excel(value, sheet = sheet)
+      a <- merge(a, b, all = TRUE)
+    }
+    num <- num - 1
+    
+  }
+  
+  attach(a)
+  
+  #this transforms the datatype of the dates column
+  a <- a %>% mutate(dates = as.Date(dates))
+  return(a)
+}
+
+data_set <- join_()
+
+write_xlsx(data_set, "C:/Users/marvi/OneDrive/Documents/Github/Stock_Markets/full_dataset.xlsx")
+
+ 
+  
